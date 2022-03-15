b1f847d2886f8f941e1d12a2ec12ef82e76f0deb
MuraFederico <federicomura98@gmail.com>
2022-03-15 15:34:28 +0100

    implementato vue, creata prima bozza struttura dati e generata la lista chat in base ai dati

---
+++ script.js
@@ -0,0 +1,99 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      userData: {
+        name: 'Federico',
+        picture: 'img/avatar_7.jpg',
+      },
+      contacts: [
+          {
+            name: 'Michele',
+            picture: 'img/avatar_1.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Fabio',
+            picture: 'img/avatar_2.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Samuele',
+            picture: 'img/avatar_3.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Alessandri B.',
+            picture: 'img/avatar_4.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Alessandro L.',
+            picture: 'img/avatar_5.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Claudia',
+            picture: 'img/avatar_6.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Sofia',
+            picture: 'img/avatar_io.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+          {
+            name: 'Davide',
+            picture: 'img/avatar_8.jpg',
+            lastAccess: '12:00',
+            chatLog: [
+                {
+                    text: '',
+                    sendTime: '',
+                }
+            ],
+          },
+      ],
+    }
+  })
